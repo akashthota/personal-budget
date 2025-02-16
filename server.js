86cdc1b12e404a02466bb7ecd995b13a1c538f9f
Meghana Thota <meghana@Akashs-MBP.lan>
2025-02-16 17:58:32 -0500

    cors

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 const port = 3000;
 
 
-app.use('/', express.static('public'));
+/* app.use('/', express.static('public')); */ 
 
 
 app.use(cors());
 
-/*const budget = {
+const budget = {
     myBudget: [
         {
             title: 'Restaurants',
@@ -32,16 +32,18 @@ app.use(cors());
         },
     ]
 };
-*/
 
-app.get('/hello', (req, res) => {
+
+
+/* app.get('/hello', (req, res) => {
    res.send("Hello World");
 });
+*/
 
-/*
+
 app.get('/budget', (req, res) => {
     res.json(budget);
- });   */
+ });   
 
  app.get('/budget', (req, res) => {
     fs.readFile('budget-data.json', 'utf8', (err, data) => {
